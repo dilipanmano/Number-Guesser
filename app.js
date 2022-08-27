ee7df3dd4265611968d28576a07d81e3f320a065
Dilipan M <dilipan12m@gmail.com>
2022-08-27 22:20:19 +0530

    Validation if input between min and max

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ let min = 1, max = 10;
 
 const minElement = document.querySelector('.min-num');
 const maxElement = document.querySelector('.max-num');
+const guessInput = document.getElementById('guess-input');
+const guessBtn = document.getElementById('guess-submit');
+const messageElement = document.getElementById('message');
 
 minElement.textContent = min;
 maxElement.textContent = max;
+
+guessBtn.addEventListener('click',validateResult);
+
+function validateResult(){
+    let inputVal = parseInt(guessInput.value);
+    if(isNaN(inputVal) || inputVal>10 || inputVal<1){
+        setMessage(`Enter a number between ${min} and ${max}`,'red');
+    }else{
+        console.log('654');
+    }
+}
+
+function setMessage(msg,clr){
+    messageElement.textContent = msg;
+    messageElement.style.color = clr
+}
