f647b78ab97f43fdc1ec24de7f07281570b2bd94
Dilipan M <dilipan12m@gmail.com>
2022-08-27 22:06:45 +0530

    set min and max from app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let min = 1, max = 10;
+
+const minElement = document.querySelector('.min-num');
+const maxElement = document.querySelector('.max-num');
+
+minElement.textContent = min;
+maxElement.textContent = max;
